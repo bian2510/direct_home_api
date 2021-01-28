5b3969f2ffe8dd66579915a4791799bd8897278a
Fabian Hernandez <bian251091@gmail.com>
2021-01-28 10:40:46 -0300

    remove seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,18 +9,3 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
-
-alias DirectHomeApi.Repo
-alias DirectHomeApi.{Address, Payment, PropertyFeatures, PropertyImages, Property, Subscription, User}
-
-  Repo.insert! %User{
-    document: 95935781,
-    document_type: "dni",
-    email: "bian251091@gmail.com",
-    last_name: "hernandez",
-    name: "carlos",
-    password: "boni2510*",
-    phone: 1173677873,
-    photo: "unafoto",
-    type: :client,
-  }
