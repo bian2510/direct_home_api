0a6405b2f3968c8b123bf2c9e0615ff4497d5e63
Pedro Matos <39782180+Petercheks@users.noreply.github.com>
2021-04-24 14:56:53 -0300

    creacion de tabla y modelo de amenities

---
+++ lib/direct_home_api/model/amenities.ex
@@ -0,0 +1,23 @@
+defmodule DirectHomeApi.Model.Amenities do
+  use Ecto.Schema
+
+  alias DirectHomeApi.Model.Property
+
+  schema "amenities" do
+    field :wifi, :boolean, default: false
+    field :pets, :boolean, default: false
+    field :pool, :boolean, default: false
+    field :childrens, :boolean, default: false
+    field :laundry, :boolean, default: false
+    field :bbq, :boolean, default: false
+    field :balcony, :boolean, default: false
+    field :porter, :boolean, default: false
+    field :gym, :boolean, default: false
+    field :parking_lot, :boolean, default: false
+
+    belongs_to :property, Property
+
+    timestamps()
+  end
+
+end
