40ef96cb7f53af0150452569333cf36a4790705a
Fabian Hernandez <fabian@covr.care>
2021-04-27 15:39:30 -0300

    test s3

--- lib/direct_home_api_web/controllers/health_controller.ex
+++ lib/direct_home_api_web/controllers/health_controller.ex
@@ -3,7 +3,7 @@ defmodule DirectHomeApiWeb.HealthController do
 
   def health_check(conn, params) do
     params["test"].path |> IO.inspect(label: "parametros")
-    ExAws.S3.put_object("dudiprops", "test.jpeg", File.read!(params["test"].path)) |> ExAws.request!() |> IO.inspect()
+    ExAws.S3.put_object("dudiprops", "testeando ando", File.read!(params["test"].path), [{:content_type, "image/jpeg"}]) |> ExAws.request!() |> IO.inspect()
     conn
     |> json(%{"status" => "available"})
   end
